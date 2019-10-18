472d9007aadf0c7182eb72845c9f2e25694689b1
Nikita Mamro <43335327+zzzloba@users.noreply.github.com>
2019-10-18 11:04:41 +0300

    Added structurized empty solution

---
+++ Lab3/FTPServer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FTPServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello from server");
+        }
+    }
+}
---
+++ Lab3/SimpleFTP/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SimpleFTP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello from SimopleFTP");
+        }
+    }
+}
