4bb68f3483dca6f079ea4da8d40019f7e742e301
Nikita Mamro <43335327+zzzloba@users.noreply.github.com>
2019-10-19 12:38:19 +0300

    Update Program.cs

--- Lab2/MyThreadPool/MyThreadPool/Program.cs
+++ Lab2/MyThreadPool/MyThreadPool/Program.cs
@@ -10,6 +10,10 @@ namespace MyThreadPool
         {
             var pool = new MyThreadPool(5);
 
+            var tasks = new List<IMyTask<int>>();
+
+            for (var i = 0; i < 50; ++i)
+            {
                 tasks.Add(pool.QueueTask(() => 2 * 2));
                 Console.WriteLine(tasks[i].Result);
                 Console.WriteLine(tasks[i].IsCompleted);
