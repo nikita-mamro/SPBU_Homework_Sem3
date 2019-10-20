dbf4374b934bc8f62f0b6393c7889936f762931f
Nikita Mamro <43335327+zzzloba@users.noreply.github.com>
2019-10-20 13:58:54 +0300

    Some useless code

---
+++ Lab3/FTPServer/FTPServer.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace FTPServer
+{
+    public class FTPServer
+    {
+        private IPAddress localAddress = IPAddress.Parse("127.0.0.1");
+        private int port;
+        private TcpListener listener;
+
+        public FTPServer(int port)
+        {
+            this.port = port;
+            listener = new TcpListener(localAddress, port);
+        }
+
+        public void Start()
+        {
+            listener.Start();
+            listener.BeginAcceptTcpClient(HandleTcpClient, listener);
+        }
+
+        public void Stop()
+        {
+            if (listener != null)
+            {
+                listener.Stop();
+            }
+        }
+
+        private void HandleTcpClient(IAsyncResult result)
+        {
+            var client = listener.EndAcceptTcpClient(result);
+            listener.BeginAcceptTcpClient(HandleTcpClient, listener);
+
+        }
+
+        private async Task List()
+        {
+
+        }
+
+        private async Task Get()
+        {
+
+        }
+    }
+}
