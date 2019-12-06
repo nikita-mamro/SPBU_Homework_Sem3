583797fe7a0bc9a30c4cbf611358a302139ed552
Nikita Mamro <43335327+zzzloba@users.noreply.github.com>
2019-12-06 23:30:08 +0300

    Added sketches for tests runner

--- Lab4/MyNUnit/MyNUnit/MyNUnit.cs
+++ Lab4/MyNUnit/MyNUnit/MyNUnit.cs
@@ -6,19 +6,62 @@ using System.Threading.Tasks;
 using MyNUnit.Attributes;
 using System.Reflection;
 using System.IO;
+using System.Collections.Concurrent;
 
 namespace MyNUnit
 {
-    public class MyNUnit
+    public static class MyNUnit
     {
-        public MyNUnit() { }
+        private static ConcurrentDictionary<Type, ConcurrentQueue<MethodInfo>> methodsToTest;
 
-        public void RunTests(string path)
+        public static void RunTests(string path)
         {
+            var classes = GetClasses(path);
 
+            foreach (var e in classes)
+            {
+                QueueClassTests(e);
+            }
+
+            ExecuteAllTests();
         }
 
-        private List<Assembly> GetAssemblies(string path)
+        private static IEnumerable<Type> GetClasses(string path)
+        {
+            var files  = Directory.EnumerateFiles(path, "*.dll");
+            var classes = files.Select(Assembly.Load)
+                .SelectMany(a => a.ExportedTypes)
+                .Where(t => t.IsClass);
+
+            return classes;
+        }
+
+        private static void QueueClassTests(Type t)
+        {
+            foreach (var method in t.GetMethods())
+            {
+                foreach (var attribute in method.GetCustomAttributes())
+                {
+                    if (attribute.GetType() == typeof(TestAttribute))
+                    {
+                        methodsToTest[t].Enqueue(method);
+                    }
+                }
+            }
+        }
+
+        private static void ExecuteAllTests()
+        {
+            Parallel.ForEach(methodsToTest.Keys, key =>
+            {
+                Parallel.ForEach(methodsToTest[key], method =>
+                {
+                    ExecuteTest(method);
+                });
+            });
+        }
+
+        private static void ExecuteTest(MethodInfo method)
         {
 
         }
